937e588a7479dab2860193ac9995b1270e79dd4a
EmnetWG <75603500+EmnetWG@users.noreply.github.com>
2025-02-06 04:13:37 -0500

    Update error-handler.js

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,14 +1,14 @@
-//const {StatusCodes} = require('http-status-codes')
-//const CustomAPIError = require('../errors/custom-error')
-//const errorHandlerMiddleware = async (err, req, res, next) => {
-//    if (err instanceof CustomAPIError) {
- //       return res.status(err.statusCode).json({ msg: err.message })
-//      }
-//      return res.status(500).send('Something went wrong try again later')
 
-//}
 
-//module.exports = errorHandlerMiddleware
+
+
+
+
+
+
+
+
+
 
 const { StatusCodes } = require('http-status-codes');
 const errorHandlerMiddleware = (err, req, res, next) => {
