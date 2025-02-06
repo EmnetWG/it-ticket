bb3fb8ea1cfcfa5f36fb948b91b07a3cbfa1c443
EmnetWG <75603500+EmnetWG@users.noreply.github.com>
2025-02-06 06:11:51 -0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -48,7 +48,7 @@ const userSchema = new mongoose.Schema ({
         const salt = await bcrypt.genSalt(10)
         this.password = await bcrypt.hash(this.password, salt)
 
-    }),
+    })
     userSchema.methods.comparePassword = async function (candidatePassword){
   const isMatch = await bcrypt.compare(candidatePassword, this.password)
   return isMatch
